2e251617ddf79b6ce1a4ba86327136ae3b095687
Marcus Lim <marcuslim136@gmail.com>
2025-09-21 09:56:04 +0800

    Updated index.js with new features

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const http = require('http');
+// const http = require('http');
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
+app.use(express.json());
 
-server.listen(port, () => {
+const helloNodeRoute = require('./routes/helloNode');
+app.use(helloNodeRoute);
+
+const serverTimeRoute = require('./routes/serverTime');
+app.use(serverTimeRoute);
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   const msg = 'Hello Node!\n'
+//   res.end(msg);
+// });
+
+const server = app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
+
+module.exports = {app, server};
