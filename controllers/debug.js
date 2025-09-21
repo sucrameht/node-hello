0062a772778c09bcf33f4f9e10dc483db9d58a14
Marcus Lim <marcuslim136@gmail.com>
2025-09-22 00:36:21 +0800

    Added debug feature

---
+++ controllers/debug.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const debug = (req, res) => {
+  const debugInfo = {
+    status: 'OK',
+    uptime: process.uptime(),
+    memoryUsage: process.memoryUsage(),
+    environment: process.env.NODE_ENV || 'development',
+    appStatus: 'Running',
+    errorLogs: []
+  };
+  res.json(debugInfo);
+};
+
+module.exports = {debug};
