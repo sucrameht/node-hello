852d984b6604dda2a55dafa9897e3f2a240a1c44
Marcus Lim <marcuslim136@gmail.com>
2025-09-21 23:17:05 +0800

    Created helloNode.html with button linking to server-time

--- controllers/helloNode.js
+++ controllers/helloNode.js
@@ -1,5 +1,7 @@
+const path = require('path');
+
 const printHelloNode = (req, res) => {
-    res.send(`Hello Node!`);
+  res.sendFile(path.join(__dirname, '../views/helloNode.html'));
 };
 
 module.exports = {printHelloNode};
